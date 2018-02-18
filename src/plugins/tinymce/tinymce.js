5204b8017b4749496bb1066cc1f9164e99c17ad3
Henrik Ingo <henrik.ingo@mongodb.com>
2018-02-18 16:22:09 +0200

    Revert "Use process.cwd() as base of js/css paths (#9)"
    This reverts commit 149c5a339f6e4635d3072c5fd3b38f4fa1d1f1c2.
    
    Using process.cwd() assumes user launched Electron from the repository
    root directory. While we use `npm start` now, this may not be the
    case eternally.

--- src/plugins/tinymce/tinymce.js
+++ src/plugins/tinymce/tinymce.js
@@ -69,7 +69,7 @@
         var html = '<div id="tinymce-toolbar"></div>';
         toolbar = impressionist().util.makeDomElement(html);
         gc.appendChild(document.body, toolbar);
-        script = impressionist().util.loadJavaScript(process.cwd() + "/node_modules/tinymce/tinymce.js", tinymceInit);
+        script = impressionist().util.loadJavaScript(process.resourcesPath + "/../../../tinymce/tinymce.js", tinymceInit);
         impressionist().gc.pushElement(script);
     });
 
